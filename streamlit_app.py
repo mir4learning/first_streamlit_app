71c448235a9b647c404fb024a06b63643c689dcf
mir4learning <126541152+mir4learning@users.noreply.github.com>
2023-02-27 23:41:05 -0500

    Update streamlit_app.py
    xyz

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit
-streamlit.title('My first dinner')
+streamlit.title('My First Restro')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
 
