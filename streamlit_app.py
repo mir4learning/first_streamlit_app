0148e50a8ba2e9b7b6fabbb5ef6f4e31308a10de
mir4learning <126541152+mir4learning@users.noreply.github.com>
2023-02-27 23:46:27 -0500

    Update streamlit_app.py
    ach

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title('My First Restro')
 
 streamlit.header('Breakfast Menu')
@@ -8,6 +10,10 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
 
